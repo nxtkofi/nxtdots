2fa32227f4750b6b53eb94476bb3e6553a11a77c
nxtkofi <psliwa.b@gmail.com>
2025-09-09 20:57:11 +0200

    feat: Add go to $PATH

--- bashrc/00-init.sh
+++ bashrc/00-init.sh
@@ -1,5 +1,6 @@
 export EDITOR=nvim
 export PATH="/usr/lib/ccache/bin/:$PATH"
 export PATH="$HOME/.local/bin:$PATH"
+export PATH="$HOME/go/bin:$PATH"
 export NVM_DIR="$HOME/.nvm"
 source /usr/share/nvm/init-nvm.sh
