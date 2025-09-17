a2eddbd79767062394250a834090b2073468b1a7
nxtkofi <psliwa.b@gmail.com>
2025-09-18 00:59:03 +0200

    feat: add initial wallpaper handling in go

---
+++ scripts/utils/pywal.go
@@ -0,0 +1,29 @@
+package utils
+
+import (
+	"os/exec"
+)
+
+func Pywal(wallpaperFilePath string) error {
+	cmd := exec.Command("gsettings", "get", "org.gnome.desktop.interface", "color-scheme")
+	output, err := cmd.Output()
+	if err != nil {
+		return err
+	}
+
+	if string(output) == "'prefer-dark'" {
+		cmd := exec.Command("wal", "-q", "-i", wallpaperFilePath)
+		_, err := cmd.Output()
+		if err != nil {
+			return err
+		}
+	} else if string(output) == "'prefer-light'" {
+		cmd := exec.Command("wal", "-q", "-l", "-i", wallpaperFilePath)
+		_, err := cmd.Output()
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+
+}
--- settings/scripts/update-cava-gradient.go
+++ settings/scripts/update-cava-gradient.go
@@ -27,37 +27,5 @@ func main() {
 		allPywalColors = append(allPywalColors, scanner.Text())
 	}
 
-	var newColors []string
-	if len(allPywalColors) >= 3 {
-		newColors = allPywalColors[0:4]
-	}
-
-	fmt.Println(newColors)
-	cavaConfig, error := os.Open(cavaConfigPath)
-	returnOnErr(error)
-
-	scanner = bufio.NewScanner(cavaConfig)
-	var cavaConfigAsArray []string
-	for scanner.Scan() {
-		cavaConfigAsArray = append(cavaConfigAsArray, scanner.Text())
-	}
-
-	for i, line := range cavaConfigAsArray {
-		if strings.HasPrefix(line, "gradient_color_1") {
-			cavaConfigAsArray[i] = "gradient_color_1 = '" + newColors[1] + "'"
-		}
-
-		if strings.HasPrefix(line, "gradient_color_2") {
-			cavaConfigAsArray[i] = "gradient_color_2 = '" + newColors[2] + "'"
-		}
-
-		if strings.HasPrefix(line, "gradient_color_3") {
-			cavaConfigAsArray[i] = "gradient_color_3 = '" + newColors[3] + "'"
-			break // Found last gradient, no need to read the rest of the file :)
-		}
-	}
-
-	output := strings.Join(cavaConfigAsArray, "\n")
-	err = os.WriteFile(cavaConfigPath, []byte(output), 0644)
-	returnOnErr(err)
+	
 }
