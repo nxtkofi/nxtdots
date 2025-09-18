e5abdb56789fc0deb57e1cd03d53ad241af41ff5
nxtkofi <psliwa.b@gmail.com>
2025-09-18 21:39:20 +0200

    wip2,fix: update main go script

--- scripts/main.go
+++ scripts/main.go
@@ -1,9 +1,8 @@
 package main
 
 import (
-	"bufio"
 	"os"
-	"scripts/utils" // Import the utils package
+	"scripts/utils"
 )
 
 func returnOnErr(err error) {
@@ -12,34 +11,22 @@ func returnOnErr(err error) {
 	}
 }
 
-func getPywalColors(pywalPath string) []string {
-	pywalFile, err := os.Open(pywalPath)
-	returnOnErr(err)
-	defer pywalFile.Close()
-
-	scanner := bufio.NewScanner(pywalFile)
-	var allPywalColors []string
-
-	for scanner.Scan() {
-		allPywalColors = append(allPywalColors, scanner.Text())
-	}
-	returnOnErr(scanner.Err())
-	return allPywalColors
-}
-
 func main() {
 	utilArg := os.Args[1]
 	wallpaperArg := os.Args[2]
-	homeDir, err := os.UserHomeDir()
-	returnOnErr(err)
-	cavaConfigPath := homeDir + "/.config/cava/config"
-	pywalColorsPath := homeDir + "/.cache/wal/colors"
-	cachePath := homeDir + "/.cache/wallpaper/"
-	var allPywalColors = getPywalColors(pywalColorsPath)
 
 	switch utilArg {
 	case "update-wallpaper":
-		err = utils.UpdateWallpaper(cachePath, wallpaperArg, cavaConfigPath, allPywalColors)
+		err := utils.UpdateWallpaper(wallpaperArg)
 		returnOnErr(err)
+	case "update-spicetify":
+		homeDir, err := os.UserHomeDir()
+		returnOnErr(err)
+
+		colors, err := utils.GetPywalColors()
+		returnOnErr(err)
+
+		err = utils.UpdateSpicetify(colors, homeDir)
+
 	}
 }
