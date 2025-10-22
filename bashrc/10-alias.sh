ced81d73b032ce82e4065241774d781067f69e0c
nxtkofi <psliwa.b@gmail.com>
2025-10-22 22:57:18 +0200

    feat: Add eza to create a better terminal experience

--- bashrc/10-alias.sh
+++ bashrc/10-alias.sh
@@ -1,12 +1,10 @@
 alias c='clear'
-alias ls='eza -a --icons=always'
+alias ls='eza --icons --color=always --group-directories-first'
 alias wifi='nmtui'
 alias vim='$EDITOR'
 
 alias shutdown='systemctl poweroff'
 
-
-alias ls='ls --color=auto'
 alias grep='grep --color=auto'
 alias cd='z'
 
