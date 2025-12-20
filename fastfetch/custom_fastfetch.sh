d37f53cc6ace370454adabc9dd6c1bb9fb9a899b
nxtkofi <psliwa.b@gmail.com>
2025-12-20 12:46:13 +0100

    feat: update logo image

--- fastfetch/custom_fastfetch.sh
+++ fastfetch/custom_fastfetch.sh
@@ -2,7 +2,7 @@
 THEME=$(gsettings get org.gnome.desktop.interface color-scheme)
 
 LOGO_LIGHT="$HOME/.config/fastfetch/logos/sentry.png"
-LOGO_DARK="$HOME/.config/fastfetch/logos/priestess.png"
+LOGO_DARK="$HOME/.config/fastfetch/logos/pretty_girl.png"
 
 LOGO_PATH=$LOGO_LIGHT
 
@@ -12,4 +12,4 @@ else
   LOGO_PATH=$LOGO_LIGHT
 fi
 
-fastfetch -c "$HOME/.config/fastfetch/config.jsonc" -l "$LOGO_PATH"
+fastfetch -c "$HOME/.config/fastfetch/config.jsonc" --logo-type kitty -l "$LOGO_PATH"
