52cef794b2dca18a734dd2a7f34be81152a13055
piotr.sliwa <piotr.sliwa@codefusion.pl>
2026-07-29 15:55:24 +0200

    feat!: nvm is no longer supported in favor of `pnpm runtime`

--- home/dot_config/bashrc/00-init.sh
+++ home/dot_config/bashrc/00-init.sh
@@ -3,8 +3,6 @@ export PATH="/usr/lib/ccache/bin/:$PATH"
 export PATH="$HOME/.local/bin:$PATH"
 export PATH="$HOME/go/bin:$PATH"
 export PATH="$HOME/.local/share/pnpm/bin:$PATH"
-export NVM_DIR="$HOME/.nvm"
-source /usr/share/nvm/init-nvm.sh
 
 bashrc_sidecar="${BASH_SOURCE[0]%.sh}.local.sh"
 [ -r "$bashrc_sidecar" ] && source "$bashrc_sidecar"
